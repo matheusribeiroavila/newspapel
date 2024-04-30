ebfebf53fe634a4e968a835829390d821a4dd971
Matbox <ribeiroavilamatheus@gmail.com>
2024-04-30 10:18:09 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,37 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Chave: "Deu bom você vai conseguir" });
+  const updateAt = new Date().toISOString();
+  const databaseCurrentVersionResult = await database.query(
+    "SELECT current_setting('server_version');",
+  );
+  const databaseCurrentVersionValue =
+    databaseCurrentVersionResult.rows[0].current_setting;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SELECT current_setting('max_connections');",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].current_setting;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseCurrentVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        current_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
